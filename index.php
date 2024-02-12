07586739050f9a753d6c4f78a2205f4654c08d53
ShockDexterity <canterw8353@my.uwstout.edu>
2024-02-11 19:37:30 -0600

    testing php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>shockdexterity.github.io</title>
+</head>
+
+<body>
+	<?php
+	echo "<h1>Hello World!</h1>";
+	echo "<p>I'm hosted with GitHub Pages.</p>";
+	?>
+</body>
+
+</html>
